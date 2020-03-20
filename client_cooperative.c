88d40392b0be7813c9f456e2c3a957b84dc4a247
Razan Said <rsaid@dons.usfca.edu>
2020-03-20 14:00:44 -0700

    update

--- client_cooperative.c
+++ client_cooperative.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <netdb.h>
 
-int main(void){
 
-	int socket_connect;
+int main(int argc, char *argv[]){
 
-	/* 
-		here we used AF_INTET to indicate we are using IPv4, SOCK_STREAM for TCP connection, and 
-	 	0 for protocol to indicate we are using IP
-	 	Note: this initializes our internet socket
-	 */
-	socket_connect = socket(AF_INET,SOCK_STREAM,0);
+	struct addrinfo init,* address_info;
+	int socket_connect, addrinfo_retrieval;
+
+	memset(&init,0,sizeof(struct addrinfo));
+
+	/*
+		first argument denotes the host we are trying to conect to (name/IP address)
+		second argument denotes the selected port of our host
+		third argument 
+	*/
+	addrinfo_retrieval= getaddrinfo(argv[0], argv[1], &init, &address_info);
+
+	if (addrinfo_retrieval != 0){
+		perror("unable to retrieve client address information\n");
+		exit(EXIT_FAILURE);
+	}
+
+	socket_connect = socket(address_info->ai_family,address_info->ai_socktype,address_info->ai_protocol);
 
 	if (socket_connect == 0){
 		perror("unable to initialize socket file descriptor\n");
--- server_cooperative.c
+++ server_cooperative.c
@@ -1,1 +1,45 @@
+/*
+	THINGS TO DO 
+	** FREE addrss_info after usage **
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+
+	struct addrinfo init,* address_info;
+	int socket_connect, addrinfo_retrieval;
+
+	memset(&init,0,sizeof(struct addrinfo));
+	init.ai_family= AF_UNSPEC;
+	init.ai_socktype=SOCK_STREAM;
+	init.ai_flags = AI_PASSIVE;
+
+	/*
+		*Argument A: denotes the host we are trying to conect to (name/IP address)
+		*Argument B: denotes the name of the service or port number we are listening on
+		*Argument C: is our iinitial address information setup
+		*Argument D: is a linked list of address information 
+		
+	*/
+	addrinfo_retrieval= getaddrinfo(NULL, argv[0], &init, &address_info);
+
+	if (addrinfo_retrieval != 0){
+		perror("unable to retrieve client address information\n");
+		exit(EXIT_FAILURE);
+	}
+
+	socket_connect = socket(address_info->ai_family,address_info->ai_socktype,address_info->ai_protocol);
+
+	if (socket_connect == 0){
+		perror("unable to initialize socket file descriptor\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+}
 
