26a7b8b0981e3ff9928a4de78ea7215097214399
djbarajas <djbarajas@dons.usfca.edu>
2020-03-20 21:02:09 -0700

    workedmore

--- read_json.c
+++ read_json.c
@@ -1,6 +1,74 @@
 
 #include "read_json.h"
 
+void set_struct(struct json * to_read, char * line)
+{
+	if (strstr(line,"Server IP") != NULL)
+	{
+		to_read->server_ip = strdup(line);
+		to_read->server_ip = to_read->server_ip + strcspn(to_read->server_ip, ":") + 3;
+		*(to_read->server_ip + strcspn(to_read->server_ip, "\"")) = '\0';
+	}
+	else if (strstr(line,"Source Port Number UDP") != NULL)
+	{
+		to_read->src_prt_udp = strdup(line);
+		to_read->src_prt_udp = to_read->src_prt_udp + strcspn(to_read->src_prt_udp, ":") + 3;
+		*(to_read->src_prt_udp + strcspn(to_read->src_prt_udp, "\"")) = '\0';
+	}
+	else if (strstr(line,"Destination Port Number UDP") != NULL)
+	{
+		to_read->dest_prt_udp = strdup(line);
+		to_read->dest_prt_udp = to_read->dest_prt_udp + strcspn(to_read->dest_prt_udp, ":") + 3;
+		*(to_read->dest_prt_udp + strcspn(to_read->dest_prt_udp, "\"")) = '\0';
+	}
+	else if (strstr(line,"Destination Port Number TCP Head") != NULL)
+	{
+		to_read->dest_prt_tcp_head = strdup(line);
+		to_read->dest_prt_tcp_head = to_read->dest_prt_tcp_head + strcspn(to_read->dest_prt_tcp_head, ":") + 3;
+		*(to_read->dest_prt_tcp_head + strcspn(to_read->dest_prt_tcp_head, "\"")) = '\0';
+	}
+	else if (strstr(line,"Destination Port Number TCP Tail") != NULL)
+	{
+		to_read->dest_prt_tcp_tail = strdup(line);
+		to_read->dest_prt_tcp_tail = to_read->dest_prt_tcp_tail + strcspn(to_read->dest_prt_tcp_tail, ":") + 3;
+		*(to_read->dest_prt_tcp_tail + strcspn(to_read->dest_prt_tcp_tail, "\"")) = '\0';
+	}
+	else if (strstr(line,"Port Number for TCP") != NULL)
+	{
+		to_read->prt_tcp = strdup(line);
+		to_read->prt_tcp = to_read->prt_tcp + strcspn(to_read->prt_tcp, ":") + 3;
+		*(to_read->prt_tcp + strcspn(to_read->prt_tcp, "\"")) = '\0';
+	}
+	else if (strstr(line,"Payload Size") != NULL)
+	{
+		char *temp = strdup(line);
+		temp = temp + strcspn(temp, ":") + 3;
+		*(temp + strcspn(temp, "\"")) = '\0';
+		to_read->payload_sz = atoi(temp);
+	}
+	else if (strstr(line,"Inter-Measurement Time") != NULL)
+	{
+		char *temp = strdup(line);
+		temp = temp + strcspn(temp, ":") + 3;
+		*(temp + strcspn(temp, "\"")) = '\0';
+		to_read->in_time = atoi(temp);
+	}
+	else if (strstr(line,"Number of UDP Packets") != NULL)
+	{
+		char *temp = strdup(line);
+		temp = temp + strcspn(temp, ":") + 3;
+		*(temp + strcspn(temp, "\"")) = '\0';
+		to_read->num_of_packets = atoi(temp);	
+	}
+	else if (strstr(line,"TTL") != NULL)
+	{
+		char *temp = strdup(line);
+		temp = temp + strcspn(temp, ":") + 3;
+		*(temp + strcspn(temp, "\"")) = '\0';
+		to_read->TTL = atoi(temp);
+	}
+}
+
 void read_json (struct json * to_read, char * file_name)
 {
 	FILE* file_ptr = NULL;
@@ -21,15 +89,25 @@ void read_json (struct json * to_read, char * file_name)
     		free(buffer);
     		break;
     	}
-    	printf("%s\n", buffer);
+    	set_struct(to_read, buffer);
     	free(buffer);
     }
 
     fclose(file_ptr);
 }
 
-int main(int argc, char ** argv)
-{
-	struct json to_read;
-	read_json(&to_read, "myconfig.json");
-}
+// int main(int argc, char ** argv)
+// {
+// 	struct json to_read;
+// 	read_json(&to_read, "myconfig.json");
+// 	printf("%s\n", to_read.server_ip);
+// 	printf("%s\n", to_read.src_prt_udp);
+// 	printf("%s\n", to_read.dest_prt_udp);
+// 	printf("%s\n", to_read.dest_prt_tcp_head);
+// 	printf("%s\n", to_read.dest_prt_tcp_tail);
+// 	printf("%s\n", to_read.prt_tcp);
+// 	printf("%d\n", to_read.payload_sz);
+// 	printf("%d\n", to_read.in_time);
+// 	printf("%d\n", to_read.num_of_packets);
+// 	printf("%d\n", to_read.TTL);
+// }
