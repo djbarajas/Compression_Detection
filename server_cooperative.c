d257b1885fc56cef748e363cb5234e736eaf11ca
djbarajas <djbarajas@dons.usfca.edu>
2020-03-27 11:43:07 -0700

    m

--- server_cooperative.c
+++ server_cooperative.c
@@ -11,7 +11,33 @@
 #include "read_json.h"
 #define SA struct sockaddr
 
+int tokenize(char * buff, struct json * tcp_info )
+{
+    char * temp;
+    tcp_info->server_ip = strsep(&buff, " ");
+    tcp_info->src_prt_udp = strsep(&buff, " ");
+    tcp_info->dest_prt_udp = strsep(&buff, " ");
+    tcp_info->dest_prt_tcp_head = strsep(&buff, " ");
+    tcp_info->dest_prt_tcp_tail = strsep(&buff, " ");
+    tcp_info->prt_tcp = strsep(&buff, " ");
+    tcp_info->payload_sz = atoi(strsep(&buff, " "));
+    tcp_info->in_time = atoi(strsep(&buff, " "));
+    tcp_info->num_of_packets = atoi(strsep(&buff, " "));
+    tcp_info->TTL = atoi(strsep(&buff, " "));
 
+    if (tcp_info->server_ip == NULL || tcp_info->src_prt_udp == NULL ||
+        tcp_info->dest_prt_udp == NULL || tcp_info->dest_prt_tcp_head == NULL ||
+        tcp_info->dest_prt_tcp_tail == NULL || tcp_info->prt_tcp == NULL ||
+        tcp_info->prt_tcp == NULL || tcp_info->payload_sz < 1 ||
+        tcp_info->in_time < 1 || tcp_info->num_of_packets < 1 ||
+        tcp_info->TTL < 0)
+    {
+        printf("NOT ENOUGH INFO CLOSING SOCKET\n");
+        return 0;
+    }
+    return 1;
+
+}
 
 
 // Function designed for chat between client and server. 
@@ -39,14 +65,15 @@ void func(int sockfd, char * buff, int len, struct json * tcp_info)
     	send(sockfd, success, 8, 0);
     	return;
     }
+}
 
-void server_setup(int socket_type, int* sockfd,struct sockaddr_in* servaddr, struct sockaddr_in* peer_addr, struct read_json){
+void server_setup(int socket_type, int* sockfd,struct sockaddr_in* servaddr, struct sockaddr_in* peer_addr){
     
     int connfd,len;
 
     // socket create and verification 
     *sockfd = socket(AF_INET, socket_type, 0); 
-    if (sockfd == -1) { 
+    if (*sockfd == -1) { 
         printf("socket creation failed...\n"); 
         exit(0); 
     } 
@@ -79,7 +106,7 @@ void server_setup(int socket_type, int* sockfd,struct sockaddr_in* servaddr, str
     len = sizeof(*peer_addr); 
   
     // Accept the data packet from client and verification 
-    connfd = accept(*sockfd, (SA*)peer_addr, peer_addr); 
+    connfd = accept(*sockfd, (SA*)peer_addr, &len); 
 
     if (connfd < 0) { 
         printf("server acccept failed...\n"); 
@@ -88,34 +115,6 @@ void server_setup(int socket_type, int* sockfd,struct sockaddr_in* servaddr, str
     else
         printf("server acccept the client...\n"); 
   
-
-}
-
-int tokenize(char * buff, struct json * tcp_info )
-{
-    char * temp;
-    tcp_info->server_ip = strsep(&buff, " ");
-    tcp_info->src_prt_udp = strsep(&buff, " ");
-    tcp_info->dest_prt_udp = strsep(&buff, " ");
-    tcp_info->dest_prt_tcp_head = strsep(&buff, " ");
-    tcp_info->dest_prt_tcp_tail = strsep(&buff, " ");
-    tcp_info->prt_tcp = strsep(&buff, " ");
-    tcp_info->payload_sz = atoi(strsep(&buff, " "));
-    tcp_info->in_time = atoi(strsep(&buff, " "));
-    tcp_info->num_of_packets = atoi(strsep(&buff, " "));
-    tcp_info->TTL = atoi(strsep(&buff, " "));
-
-    if (tcp_info->server_ip == NULL || tcp_info->src_prt_udp == NULL ||
-        tcp_info->dest_prt_udp == NULL || tcp_info->dest_prt_tcp_head == NULL ||
-        tcp_info->dest_prt_tcp_tail == NULL || tcp_info->prt_tcp == NULL ||
-        tcp_info->prt_tcp == NULL || tcp_info->payload_sz < 1 ||
-        tcp_info->in_time < 1 || tcp_info->num_of_packets < 1 ||
-        tcp_info->TTL < 0)
-    {
-        printf("NOT ENOUGH INFO CLOSING SOCKET\n");
-        return 0;
-    }
-    return 1;
 
 }
 
@@ -129,17 +128,17 @@ int main()
     struct json tcp_info; 
     char buff[1000];
   
-
+    server_setup(SOCK_STREAM,&sockfd,&servaddr,&cli);
     //setup and open new connection for server (we are currently expecting UDP packets that divide to two parts)    
-    server_setup(SOCK_DGRAM,&sockfd,&servaddr,&peer_addr);
-    // we want to recieve the low entropy packet train (Quantity: 6000)    
-    for (int i=0;i<6000;i++){        
-        recvfrom(sockfd, udp_payload, sizeof(udp_payload),0, &recvd_sock,sizeof(recvd_sock));
-    }
-    // now we want to recieve the high entropy packet train (Quantity: 6000)    
-    for (int i=0;i<6000;i++){        
-        recvfrom(sockfd, udp_payload, sizeof(udp_payload),0, &recvd_sock,sizeof(recvd_sock));
-    }
+    // server_setup(SOCK_DGRAM,&sockfd,&servaddr,&peer_addr);
+    // // we want to recieve the low entropy packet train (Quantity: 6000)    
+    // for (int i=0;i<6000;i++){        
+    //     recvfrom(sockfd, udp_payload, sizeof(udp_payload),0, &recvd_sock,sizeof(recvd_sock));
+    // }
+    // // now we want to recieve the high entropy packet train (Quantity: 6000)    
+    // for (int i=0;i<6000;i++){        
+    //     recvfrom(sockfd, udp_payload, sizeof(udp_payload),0, &recvd_sock,sizeof(recvd_sock));
+    // }
 
     close(sockfd);
 } 
