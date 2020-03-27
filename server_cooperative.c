8ea8cae67b60aa3a15c8c363c096c10fdb55f758
Razan Said <rsaid@dons.usfca.edu>
2020-03-27 11:04:08 -0700

    commit

--- server_cooperative.c
+++ server_cooperative.c
@@ -161,80 +161,18 @@ int main()
     struct json tcp_info; 
     char buff[1000];
   
-    // socket create and verification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(8081); 
-  
-    // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully binded..\n"); 
-  
-    // Now server is ready to listen and verification 
-    if ((listen(sockfd, 5)) != 0) { 
-        printf("Listen failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Server listening..\n"); 
-    len = sizeof(cli); 
-  
-    // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (SA*)&cli, &len); 
-    if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("server acccept the client...\n"); 
-  
-    // Function for chatting between client and server 
-    func(connfd, buff, 1000, &tcp_info); 
-        
 
-
-    // After chatting close the socket 
-    close(sockfd);
-
-    //UDP STUFF
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(atoi(tcp_info.dest_prt_udp)); 
-  
-    // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully binded..\n"); 
-  
-
-
+    //setup and open new connection for server (we are currently expecting UDP packets that divide to two parts)    
+    server_setup(SOCK_DGRAM,&sockfd,&servaddr,&peer_addr);
+    // we want to recieve the low entropy packet train (Quantity: 6000)    
+    for (int i=0;i<6000;i++){        
+        recvfrom(sockfd, udp_payload, sizeof(udp_payload),0, &recvd_sock,sizeof(recvd_sock));
+    }
+    // now we want to recieve the high entropy packet train (Quantity: 6000)    
+    for (int i=0;i<6000;i++){        
+        recvfrom(sockfd, udp_payload, sizeof(udp_payload),0, &recvd_sock,sizeof(recvd_sock));
+    }
+    
     close(sockfd);
 } 
 
