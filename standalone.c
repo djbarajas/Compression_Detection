3bb3216e318c5555da0837e27cf8235f110b4a38
razsaid <42684083+razsaid@users.noreply.github.com>
2020-03-31 12:04:32 -0700

    update

--- standalone.c
+++ standalone.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h> 
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <netinet/ip.h>
+#include <arpa/inet.h> 
+#include <errno.h>
 #include "read_json.h"
 
 /*
