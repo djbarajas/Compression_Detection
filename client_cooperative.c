10156e79b170890e1a318c94e9fe6c21b7da3bf5
Daniel Barajas <35737585+djbarajas@users.noreply.github.com>
2020-04-07 17:00:19 -0700

    c

--- client_cooperative.c
+++ client_cooperative.c
@@ -33,14 +33,9 @@ void read_high_entropy_data(uint8_t * data, int len){
     file_ptr =  fopen("/dev/random", "r");
     for (int i = 0; i < len; i++){
         temp = getc(file_ptr);
-        int t = atoi(&temp);
-        if (t > 1){
-            t = 1;
-        }
-        data[i] = t;
+        data[i] = temp;
     }
-
-
+    fclose(file_ptr);
 }
 
 void packet_setup(struct json packet_info, int socket_type, int* sockfd,struct sockaddr_in* clientaddr){
@@ -133,7 +128,7 @@ void send_packet_train(struct json* packet_info)
     close(sockfd); 
 }
   
-int main() 
+int main(int argc, char **argv) 
 { 
     char binary[16];
   
@@ -145,11 +140,28 @@ int main()
     int sockfd, connfd, val, clientlen; 
     struct sockaddr_in clientaddr, cli;
     struct json packet_info;
-    uint8_t *data;
+    uint8_t *data, *data_2;
     
 
     char buff[1000] = {0};
-    read_json(&packet_info, "myconfig.json", buff); 
+
+    if (argc != 2){
+      fprintf (stderr, "ERROR: Too few or many arguments.\n");
+      exit (EXIT_FAILURE);
+    }
+
+    data = allocate_ustrmem(packet_info.payload_sz);
+
+    data_2 = allocate_ustrmem(packet_info.payload_sz);
+
+    bzero(data, packet_info.payload_sz);
+
+    bzero(data_2, packet_info.payload_sz);
+
+    read_high_entropy_data(&data_2[16], packet_info.payload_sz-16);
+    printf("Finished reading high entropy data\n");
+
+    read_json(&packet_info, argv[1], buff); 
 
     packet_setup(packet_info, SOCK_STREAM, &sockfd, &clientaddr);
 
@@ -185,7 +197,7 @@ int main()
     /**
         Probing phase [send in UDP packet trains of high and low entropy data each of quantity 6000] 
     */
-    sleep (25);
+    sleep (20);
 
     //send_packet_train(&packet_info);
     struct sockaddr_in addr, srcaddr;
@@ -227,7 +239,6 @@ int main()
 
     //let's setup UDP low entropy payload 
 
-    data = allocate_ustrmem(packet_info.payload_sz);
     clientlen = sizeof(addr);
 
 
@@ -239,14 +250,10 @@ int main()
     }
 
     sleep(packet_info.in_time);
-
-    bzero(data, packet_info.payload_sz);
-
-    read_high_entropy_data(&data[16], packet_info.payload_sz-16);
-
+    
     for (int i=0;i<packet_info.num_of_packets;i++){
-        packet_id_setup(data, packet_id++);
-        if(sendto(fd,data,packet_info.payload_sz,MSG_CONFIRM,(struct sockaddr *) &addr,clientlen)<=0){
+        packet_id_setup(data_2, packet_id++);
+        if(sendto(fd,data_2,packet_info.payload_sz,MSG_CONFIRM,(struct sockaddr *) &addr,clientlen)<=0){
             continue;
         }  
     }
@@ -292,5 +299,8 @@ int main()
     bzero(buff, 1000); 
     recv(sockfd, buff, 50, 0);
     printf("%s\n",buff); 
-} 
 
+    free(data);
+    free(data_2);
+
+} 
